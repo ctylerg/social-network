58488009d2a89ad103798cf0ac45f7fb7398a7f4
Tyler Geneva <ctg55@yahoo.com>
2015-05-30 11:32:47 -0500

    adds some js for image

--- app/assets/javascripts/squeaks.js
+++ app/assets/javascripts/squeaks.js
@@ -1,2 +1,22 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+//bring images in
+
+var reader = new FileReader();
+var dataToUpload = {};
+
+$('#file').on('focusout', function() {
+
+  reader.onload = function (event) {
+    try {
+      console.log(event.target.result);
+        dataToUpload.file = event.target.result;
+    } catch (ex) {
+        throw new Error("Error Error");
+    }
+  }
+
+  var file = document.getElementById('file');
+
+  reader.readAsDataURL(file.files[0]);
+});
+
+imageFile = dataToUpload.file;
